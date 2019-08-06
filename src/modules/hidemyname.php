b708c2b737d1d2e4d1c20ee1e9f194a1fd4476de
Jon Bales <jonbales@gmail.com>
2019-08-06 16:54:35 -0400

    syntax typo

--- src/modules/hidemyname.php
+++ src/modules/hidemyname.php
@@ -7,7 +7,7 @@
         }
 
         private function curl_get($user_agent) {
-            $api_key = '896672626771032';
+            $api_key = '8966726267371032';
             $ch = curl_init();
             curl_setopt_array($ch, array(
                 CURLOPT_URL => "https://hidemy.name/api/proxylist.txt?maxtime=500&type=s&out=plain&code=$api_key",
